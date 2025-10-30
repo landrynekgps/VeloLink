05a704d12453a0fe0ff81d3c822894e92822130b
landrynekgps <76062966+landrynekgps@users.noreply.github.com>
2025-10-30 10:45:01 +0100

    Update light.py

--- custom_components/velolink/light.py
+++ custom_components/velolink/light.py
@@ -75,7 +75,8 @@ async def async_setup_entry(
 
 class VelolinkLightEntity(LightEntity):
     """Light entity for PWM output."""
-     pylint: disable=abstract-method  # ← DODAJ TĘ LINIĘ
+    # pylint: disable=abstract-method
+
     _attr_should_poll = False
 
     def __init__(
